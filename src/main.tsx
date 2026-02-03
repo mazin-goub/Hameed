957ed21e5b41982a2dbe0ae2bfa2f63b1be29d47
Mazin Mahgoub <mazinmahgoub91@gmail.com>
2026-02-03 21:52:28 +0200

    fix convex url

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { createRoot } from "react-dom/client";
 import { ConvexAuthProvider } from "@convex-dev/auth/react";
-import { ConvexReactClient } from "convex/react";
 import "./index.css";
 import App from "./App";
 
-const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
+// const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
+
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+
+const convexUrl = import.meta.env.VITE_CONVEX_URL;
+const convex = new ConvexReactClient(convexUrl);
+
+<ConvexProvider client={convex}>
+  <App />
+</ConvexProvider>
 
 createRoot(document.getElementById("root")!).render(
   <ConvexAuthProvider client={convex}>
